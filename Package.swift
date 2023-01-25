5ffa65f4dfeacf040e15d1f4662a4723a5aac7c7
Denis Mikaya <denis.mikaya@unblu.com>
2023-01-25 10:28:30 +0100

    added LiveKit module

--- Package.swift
+++ Package.swift
@@ -6,7 +6,8 @@ let version = "4.5.5"
 
 let UnbluCoreSDK_CHECKSUM = "89131e09f351be18b84b2aa24c04070812ca26899bbc625931ce86371eb437fc"
 let UnbluFirebaseNotificationModule_CHECKSUM = "409cb55429c4e61b30bba010e3f96f2a3fdbb0f64c4a3aa46404b9144d9f587e"
-let UnbluCallModule_CHECKSUM = "dcba8246c85ed9424e0b4785914565f2c9ee96bfa248fb94cec09707cb13d878"
+let UnbluOpenTokCallModule_CHECKSUM = "dcba8246c85ed9424e0b4785914565f2c9ee96bfa248fb94cec09707cb13d878"
+let UnbluLiveKitCallModule_CHECKSUM = "dcba8246c85ed9424e0b4785914565f2c9ee96bfa248fb94cec09707cb13d878"
 let UnbluMobileCoBrowsing_CHECKSUM = "c53a9470638830e3345fb997d4f916c303da9a9936fd8deef0a7a073f96f7cc6"
 
 
@@ -18,7 +19,7 @@ let package = Package(
     products: [
         .library(
             name: "UnbluMobileSDK",
-            targets: ["UnbluCoreSDK","UnbluFirebaseNotificationModule","UnbluCallModule","UnbluMobileCoBrowsingModule"]),
+            targets: ["UnbluCoreSDK","UnbluFirebaseNotificationModule","UnbluOpenTokCallModule","UnbluLiveKitCallModule","UnbluMobileCoBrowsingModule"]),
     ],
     dependencies: [
         .package(name: "WebRTC", url: "https://github.com/webrtc-sdk/Specs.git", .exact("104.5112.08"))
@@ -35,9 +36,14 @@ let package = Package(
             checksum: UnbluFirebaseNotificationModule_CHECKSUM
             ),
         .binaryTarget(
-            name: "UnbluCallModule",
-            url: "https://github.com/unblu/ios-sdk-xcframeworks/blob/\(version)/UnbluCallModule.xcframework.zip?raw=true",
-            checksum: UnbluCallModule_CHECKSUM
+            name: "UnbluOpenTokCallModule",
+            url: "https://github.com/unblu/ios-sdk-xcframeworks/blob/\(version)/UnbluOpenTokCallModule.xcframework.zip?raw=true",
+            checksum: UnbluOpenTokCallModule_CHECKSUM
+            ),
+        .binaryTarget(
+            name: "UnbluLiveKitCallModule",
+            url: "https://github.com/unblu/ios-sdk-xcframeworks/blob/\(version)/UnbluLiveKitCallModule.xcframework.zip?raw=true",
+            checksum: UnbluLiveKitCallModule_CHECKSUM
             ),
         .binaryTarget(
             name: "UnbluMobileCoBrowsingModule",
