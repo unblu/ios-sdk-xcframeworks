316939361673c42ea97c773396f559efba4cf160
Denis Mikaya <denis.mikaya@unblu.com>
2023-02-06 14:45:03 +0100

    update Package.swift for version 4.5.10

--- Package.swift
+++ Package.swift
@@ -2,13 +2,13 @@
 
 import PackageDescription
 
-let version = "4.5.14"
+let version = "4.5.10"
 
-let UnbluCoreSDK_CHECKSUM = "324bfe837fb0ffa7473e7028610da786656022feaad2e31d5a6b14b110585ba1"
-let UnbluFirebaseNotificationModule_CHECKSUM = "674f350b812d782b640ae59b273790f8139655a4ffd25485401a71182e5012cf"
-let UnbluOpenTokCallModule_CHECKSUM = "ad56431bbeca76862d563bd583ed01bf403a296de0caa576fa0a7292d2ec7858"
-let UnbluLiveKitCallModule_CHECKSUM = "ac430de40a846c6651d6589117e560bd4d39c487d8e4830194aa87501f9d10eb"
-let UnbluMobileCoBrowsing_CHECKSUM = "bfe247f3bb268a724d3397e15f1f9808704527915dfe021c9f1c92c8c98a0a0f"
+let UnbluCoreSDK_CHECKSUM = "61cda7c8d2229d5496809374677820ef3f25cc1583bedbaefd6145c231342f31"
+let UnbluFirebaseNotificationModule_CHECKSUM = "b1ae286fd2dbd2c6e61b17c5a3e0ca485b0bf91576e3a2ff6fc1ef6808a33d38"
+let UnbluOpenTokCallModule_CHECKSUM = "1f58c55aeca4cdf2afebff65e7447fdf48556d0ca70c6833c749210476942a75"
+let UnbluLiveKitCallModule_CHECKSUM = "329bb1b4ded5785dbba954ed19c9176b1dad230f3c6a4a6857126b35aca740de"
+let UnbluMobileCoBrowsing_CHECKSUM = "6cbd154540b1f9e04dc17aea120cda5e26689c3b12ae467d23ae86fcfe627ec6"
 
 
 let package = Package(
