73c037f7267dffc0e9c18aa6ddfd231b85356a79
Denis Mikaya <denis.mikaya@unblu.com>
2023-03-04 18:16:59 +0100

    ...

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let UnbluFirebaseNotificationModule_CHECKSUM = "24cb63cc16589a22525ccc85a6fc788f
 let UnbluOpenTokCallModule_CHECKSUM = "662284dac9d66969ad7cfad94a2b50c2eefab0953ab9ed45174465baac231492"
 let UnbluLiveKitCallModule_CHECKSUM = "8387b6201e73d3c4d8bf54abb2a5b7e5b9697ca91e80f984c34fd34b5d29438b"
 let UnbluMobileCoBrowsing_CHECKSUM = "e864332dac9363d8c9f67a28a4c3a03c11c4306d1d3016073fde99d3e5bfd65d"
+let FirebaseMessaging_CHECKSUM = "e864332dac9363d8c9f67a28a4c3a03c11c4306d1d3016073fde99d3e5bfd65d"
 
 
 let package = Package(
@@ -19,7 +20,7 @@ let package = Package(
     products: [
         .library(
             name: "UnbluMobileSDK",
-            targets: ["UnbluCoreSDK","UnbluFirebaseNotificationModule","UnbluMobileCoBrowsingModule"]),
+            targets: ["UnbluCoreSDK","UnbluFirebaseNotificationModule","UnbluMobileCoBrowsingModule","FirebaseMessaging"]),
         .library(
             name: "UnbliLiveKitWebRtcProvider",
             targets: ["UnbluLiveKitCallModule"]),
@@ -52,6 +53,11 @@ let package = Package(
             name: "UnbluMobileCoBrowsingModule",
             url: "https://github.com/unblu/ios-sdk-xcframeworks/blob/\(version)/UnbluMobileCoBrowsingModule.xcframework.zip?raw=true",
             checksum: UnbluMobileCoBrowsing_CHECKSUM
+            )
+        .binaryTarget(
+            name: "FirebaseMessaging",
+            url: "https://github.com/unblu/ios-sdk-xcframeworks/blob/\(version)/FirebaseMessaging.xcframework.zip?raw=true",
+            checksum: FirebaseMessaging_CHECKSUM
             )
     ]
 )
