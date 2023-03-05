8aa9e35662533ee73c0643a422fa579a0948ec50
Denis Mikaya <denis.mikaya@unblu.com>
2023-03-05 11:37:59 +0100

    ...

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let version = "main"
 
 let UnbluCoreSDK_CHECKSUM = "425e1196e3d0f702c6eb04ce77ed6f6e35b3d4a70666cf8f402b5f0911cd537d"
-let UnbluFirebaseNotificationModule_CHECKSUM = "56f52675412235c928151e6472982ba3980a9f04bf9397b45998c3562526ab2d"
+let UnbluFirebaseNotificationModule_CHECKSUM = "966002aebb78958369e4a8da94bfd041be654dcfbf10e646cf664635f0b4028b"
 let UnbluOpenTokCallModule_CHECKSUM = "662284dac9d66969ad7cfad94a2b50c2eefab0953ab9ed45174465baac231492"
 let UnbluLiveKitCallModule_CHECKSUM = "8387b6201e73d3c4d8bf54abb2a5b7e5b9697ca91e80f984c34fd34b5d29438b"
 let UnbluMobileCoBrowsing_CHECKSUM = "e864332dac9363d8c9f67a28a4c3a03c11c4306d1d3016073fde99d3e5bfd65d"
