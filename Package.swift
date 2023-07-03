ff18edcbc01c828738843547d2616e7f93dc339a
denis.trueby@unblu.com <jenkins@jenkins-worker-osx.local>
2023-07-03 10:46:26 +0100

    update frameworks

--- Package.swift
+++ Package.swift
@@ -2,14 +2,14 @@
 
 import PackageDescription
 
-let version = "4.7.2"
+let version = "main"
 
-let UnbluCoreSDK_CHECKSUM = "fb4ca0e8c063585ca19f269f8d586a0dddcb81f0e053e92c9016fe532495c5f1"
-let UnbluFirebaseNotificationModule_CHECKSUM = "81112e6255277bd078267999f1f8f7ce5640ca12c2bdd86e683fc1d78d66766d"
-let UnbluOpenTokCallModule_CHECKSUM = "0a3b0a1fd90c4336350543aea9107eb59c8885193ef5e3642ad204c8af84c471"
-let UnbluLiveKitCallModule_CHECKSUM = "209bd198e8d6dd20ed190f762b6e63917458f469bebc94183d66c2e0b35c66e7"
-let UnbluMobileCoBrowsing_CHECKSUM = "c6be06d5272f1caeee5180eaef83e51e94a1f3c0e44b90c1114d7d02f10a5fb5"
-let UnbluCallKitModule_CHECKSUM = "45d22ee866bab8abd656601d106c75fb192780622f30611e5c81d1732acf6c8f"
+let UnbluCoreSDK_CHECKSUM = "e8100d3b811bdeb55eb425d108b19e8d66ecb5f631fe25b5725924c65862dda2"
+let UnbluFirebaseNotificationModule_CHECKSUM = "742043885fed45e9f65bd73fd4482f7b1f0d13f8cb4515179f7a787b3da53ed8"
+let UnbluOpenTokCallModule_CHECKSUM = "fb3dae06a719123db260d092fdb9d4dffeaf29f995df34cb2ac430991e76e767"
+let UnbluLiveKitCallModule_CHECKSUM = "48bcf14944462eedc9ea026a370fbced322eb279f9c39225492d45a2cfe21736"
+let UnbluMobileCoBrowsing_CHECKSUM = "08d8bab4929b428cdde3dc88ee7d5a55e4761546184396056d42f06ca2e45968"
+let UnbluCallKitModule_CHECKSUM = "a36c0f748cf0cb6a3f6e05af381854d8810df7ba51c7b121914eacbd9f5c1475"
 
 
 
